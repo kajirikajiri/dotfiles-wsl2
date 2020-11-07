397d1cbd05f1d1050b3867079721864385d9e099
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-11-08 03:53:23 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -39,50 +39,50 @@ done
 
 # zshがなければinstallする
 if has "zsh"; then
-  echo
-  echo
+  echo -e "\n"
+  echo -e "\n"
   echo 'zsh is present!'
 else
   sudo apt update
   sudo apt install -y zsh
   chsh -s $(which zsh) || true # for skipping in CI
-  echo
-  echo
+  echo -e "\n"
+  echo -e "\n"
   echo 'installed zsh'
 fi
 
 # golangがなければinstallする
 if has "go"; then
-  echo
-  echo
+  echo -e "\n"
+  echo -e "\n"
   echo 'go is present!'
 else
   sudo add-apt-repository ppa:longsleep/golang-backports
   sudo apt update
   sudo apt install -y golang-go
   go get github.com/x-motemen/ghq
-  echo
-  echo
+  echo -e "\n"
+  echo -e "\n"
   echo 'installed go'
 fi
 
 # fzfがなければinstallする
 if has "fzf"; then
-  echo
-  echo
+  echo -e "\n"
+  echo -e "\n"
   echo 'fzf is present!'
 else
   sudo apt-get update
   sudo apt-get install -y fzf
-  echo
-  echo
+  echo -e "\n"
+  echo -e "\n"
   echo 'installed fzf'
 fi
 
 # xeyes を使えるようにする
 if has "google-chrome"; then
-  echo
-  echo
+  echo -e "\n"
+  echo -e "\n"
   echo 'google-chrome present!'
 else
   sudo apt update && sudo apt upgrade -y
@@ -93,26 +93,28 @@ else
   sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
   sudo apt update
   sudo apt install -y google-chrome-stable
-  echo
-  echo
+  echo -e "\n"
+  echo -e "\n"
   echo 'installed google-chrome'
 fi
 
 # hub を使えるようにする
 if has "hub"; then
-  echo
-  echo
+  echo -e "\n"
+  echo -e "\n"
   echo 'hub present!'
 else
   sudo apt update
   sudo apt install -y hub
-  echo
-  echo
+  echo -e "\n"
+  echo -e "\n"
   echo 'installed hub'
 fi
 
 # yarn を使えるようにする
 if has "yarn"; then
+  echo -e "\n"
+  echo -e "\n"
   echo 'yarn present!'
 else
   sudo apt install -y nodejs npm
@@ -123,5 +125,7 @@ else
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
   sudo apt update && sudo apt install -y yarn
   sudo apt purge -fy libuv1 nodejs
+  echo -e "\n"
+  echo -e "\n"
   echo 'installed yarn'
 fi
