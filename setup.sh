0e0c66923e290156cff5c17a14080ec687f1e274
kazuki <37785038+kajirikajiri@users.noreply.github.com>
2020-11-08 04:51:40 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -37,18 +37,6 @@ do
     ln -snfv "$DOTPATH/$f" "$HOME/$f"
 done
 
-if has "sudo"; then
-  echo -e "\n"
-  echo 'sudo is present!'
-  echo -e "\n"
-elif has "apt"; then
-  apt update
-  apt install -y sudo
-  echo -e "\n"
-  echo 'install sudo'
-  echo -e "\n"
-fi
-
 # zshがなければinstallする
 if has "zsh"; then
   echo -e "\n"
