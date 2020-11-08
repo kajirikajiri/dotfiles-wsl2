6fcd50c69f871b1d735fea678523472ca2e4cd65
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-11-08 18:17:48 +0900

    fix

--- .zsh.d/0003-style.zsh
+++ .zsh.d/0003-style.zsh
@@ -1,11 +1,53 @@
-# Load version control information
-autoload -Uz vcs_info
-precmd() { vcs_info }
+# set light prompt
+# git ブランチ名を色付きで表示させるメソッド
+function rprompt-git-current-branch {
+  local branch_name st branch_status
+  
+  branch='\ue0a0'
+  color='%{\e[38;5;' #  文字色を設定
+  green='114m%}'
+  red='001m%}'
+  yellow='227m%}'
+  blue='033m%}'
+  reset='%{\e[0m%}'   # reset
+  # ブランチマーク
+  # ${branch}の後にスーペースを入れる
+  if [ ! -e  ".git" ]; then
+      # git 管理されていないディレクトリは何も返さない
+    return
+  fi
+  branch_name=`git rev-parse --abbrev-ref HEAD 2> /dev/null`
+  st=`git status 2> /dev/null`
+  if [[ -n `echo "$st" | grep "^nothing to"` ]]; then
+    # 全て commit されてクリーンな状態
+    branch_status="${color}${green}${branch}　"
+  elif [[ -n `echo "$st" | grep "^Untracked files"` ]]; then
+    # git 管理されていないファイルがある状態
+    branch_status="${color}${red}${branch}　?"
+  elif [[ -n `echo "$st" | grep "^Changes not staged for commit"` ]]; then
+    # git add されていないファイルがある状態
+    branch_status="${color}${red}${branch}　+"
+  elif [[ -n `echo "$st" | grep "^Changes to be committed"` ]]; then
+    # git commit されていないファイルがある状態
+    branch_status="${color}${yellow}${branch}　!"
+  elif [[ -n `echo "$st" | grep "^rebase in progress"` ]]; then
+    # コンフリクトが起こった状態
+    echo "${color}${red}${branch}　!(no branch)${reset}"
+    return
+  else
+    # 上記以外の状態の場合
+    branch_status="${color}${blue}${branch}"
+  fi
+  # ブランチ名を色付きで表示する
+  echo "${branch_status}$branch_name${reset}"
+}
 
-# Format the vcs_info_msg_0_ variable
-zstyle ':vcs_info:git:*' formats 'on branch %b'
+# プロンプトが表示されるたびにプロンプト文字列を評価、置換する
+setopt prompt_subst
  
-# Set up the prompt (with git branch name)
-setopt PROMPT_SUBST
-PROMPT='%n in ${PWD/#$HOME/~} > '
-RPROMPT=\$vcs_info_msg_0_
+# プロンプトの右側にメソッドの結果を表示させる
+RPROMPT='`rprompt-git-current-branch`'
+
+# set left prompt
+PROMPT='${PWD/#$HOME/~}
+> '
