4f36958fea8f26ea69d3b7c549188a6ea1fce199
kajirikajiri <kajirikajiri@gmail.com>
2021-01-03 03:49:49 +0900

    add yarn global path

--- .zsh.d/0001-path.zsh
+++ .zsh.d/0001-path.zsh
@@ -1,4 +1,5 @@
 export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
 export GOPATH=$HOME/go
-export PATH=$GOPATH/bin:$PATH
+export YARN_GLOBAL=$(yarn global bin)
+export PATH=$GOPATH/bin:$YARN_GLOBAL:$PATH
 
