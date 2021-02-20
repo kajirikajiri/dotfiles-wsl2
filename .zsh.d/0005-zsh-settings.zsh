e8f9e0b5b6b08992a383def96c30881556c69371
kajirikajiri <kajirikajiri@gmail.com>
2021-02-20 18:21:51 +0900

    :sparkles: (.zsh.d/0005-zsh-settings.zsh) add export

--- .zsh.d/0005-zsh-settings.zsh
+++ .zsh.d/0005-zsh-settings.zsh
@@ -1,4 +1,11 @@
-SAVEHIST=100
-HISTFILE=~/.zsh_history
+# メモリに保存される履歴の件数
+export HISTSIZE=1000
+# 履歴ファイルに保存される履歴の件数
+export SAVEHIST=100000
+# 履歴ファイルの場所
+export HISTFILE=~/.zsh_history
+# ?
+setopt EXTENDED_HISTORY
+# ?
 setopt share_history
 
